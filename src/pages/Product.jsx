6e1cd336101d2b61d59c6c267b6d751ac044a743
DeepMakhasana <deepmakhasana1013@gmail.com>
2024-07-01 16:22:47 +0530

    more new product add

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,6 +6,7 @@ const Product = () => {
                 <h2>Product - 1</h2>
                 <h2>Product - 2</h2>
                 <h2>Product - 3</h2>
+                <h3>product-1</h3>
             </div>
         </div>
     )
